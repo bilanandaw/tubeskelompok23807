2ea2639490fc2ea518d68766f05bc5e0c67ec6c9
7 <7@7-PC>
2016-03-30 20:00:41 +0700

    Driver_Nabila

---
+++ Driver/src/driver/Driver.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package driver;
+
+/**
+ *
+ * @author 7
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Aplikasi aplikasiNya = new Aplikasi();
+       aplikasiNya.mainMenu();
+    }
+    
+}
